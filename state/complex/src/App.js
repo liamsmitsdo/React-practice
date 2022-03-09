9e4ad066e615cc4ea81887361da7d29d46127c65
liamsmitsdo <liamsmitsdorff@gmail.com>
2022-03-09 15:27:19 +0200

    finished state section

--- state/complex/src/App.js
+++ state/complex/src/App.js
@@ -13,6 +13,17 @@ const InstructorItem = props => { //stateless functional component
     );
 }
 
+const Box = ({color}) => {
+  const style = {
+    width: '200px',
+    height: '200px',
+    display: 'inline-block',
+    margin: '5px',
+    backgroundColor: color
+  }
+  return <div style={style}></div>
+};
+
 InstructorItem.propTypes = { //updating the proptypes
   name: PropTypes.string,
   hobbies: PropTypes.arrayOf(PropTypes.string)
@@ -22,62 +33,87 @@ InstructorItem.propTypes = { //updating the proptypes
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      instructors: [
-        {
-          name: 'Tim',
-          hobbies: ['sailing', 'react']
-        }, {
-          name: 'Matt',
-          hobbies: ['math', 'd3']
-        }, {
-          name: 'Colt',
-          hobbies: ['css', 'hiking']
-        }, {
-          name: 'Elie',
-          hobbies: ['music', 'es2015']
-        }
-      ]
-    };
+    const boxes = Array(14).fill().map(this.getColor, this);
+    // this.state = {
+    //   instructors: [
+    //     {
+    //       name: 'Tim',
+    //       hobbies: ['sailing', 'react']
+    //     }, {
+    //       name: 'Matt',
+    //       hobbies: ['math', 'd3']
+    //     }, {
+    //       name: 'Colt',
+    //       hobbies: ['css', 'hiking']
+    //     }, {
+    //       name: 'Elie',
+    //       hobbies: ['music', 'es2015']
+    //     }
+    //   ]
+    // };
+    this.state = {boxes}
+    setInterval(()=>{
+      const boxes = this.state.boxes.slice();
+      const randInd = Math.floor(Math.random() * boxes.length);
+      boxes[randInd] = this.getColor();
+      this.setState({boxes})
+    },300);
 
-    setTimeout(()=>{
-      const randInst = Math.floor(Math.random() * this.state.instructors.length);
-      const hobbyIdx = Math.floor(Math.random() * this.state.instructors[randInst].length);
+    // setTimeout(()=>{
+    //   const randInst = Math.floor(Math.random() * this.state.instructors.length);
+    //   const hobbyIdx = Math.floor(Math.random() * this.state.instructors[randInst].length);
 
-      const instructors = this.state.instructors.map((inst, i) => {
-        if(i === randInst){
-          const hobbies = [...inst.hobbies];
-          hobbies.splice(hobbyIdx, 1);
-          return {
-            ...inst,
-            hobbies
-          };
-        }
+    //   const instructors = this.state.instructors.map((inst, i) => {
+    //     if(i === randInst){
+    //       const hobbies = [...inst.hobbies];
+    //       hobbies.splice(hobbyIdx, 1);
+    //       return {
+    //         ...inst,
+    //         hobbies
+    //       };
+    //     }
 
-        return inst;
-      });
-      this.setState({instructors})
-      // instructors[randInst] = Object.assign({}, instructors[randInst]);
-      // instructors[randInst].hobbies = instructors[randInst].hobbies.slice();
-      // instructors[randInst].hobbies.splice(hobbyIdx, 1);
-    }, 5000);
+    //     return inst;
+    //   });
+    //   this.setState({instructors})
+    //   // instructors[randInst] = Object.assign({}, instructors[randInst]);
+    //   // instructors[randInst].hobbies = instructors[randInst].hobbies.slice();
+    //   // instructors[randInst].hobbies.splice(hobbyIdx, 1);
+    // }, 5000);
   }
+  getColor(){
+    let index = Math.floor(Math.random() * this.props.colors.length);
+    return this.props.colors[index];
+  }
+  
   render() {
-    const instructors = this.state.instructors.map((instructor, index) => (
-      <InstructorItem 
-      key={index} 
-      name={instructor.name}
-      hobbies={instructor.hobbies}
-      />
+    // const instructors = this.state.instructors.map((instructor, index) => (
+    //   <InstructorItem 
+    //   key={index} 
+    //   name={instructor.name}
+    //   hobbies={instructor.hobbies}
+    //   />
+    // ));
+    const boxes = this.state.boxes.map((b, i) => (
+      <Box key={i} color={b}/>
     ));
     return (
       <div className="App">
-        <ul>
-          {instructors}
-        </ul>
+        <div>
+          {boxes}
+        </div>
       </div>
     );
   }
+}
+
+App.defaultProps = {
+    colors: [
+      "blue",
+      "green",
+      "yellow",
+      "red"
+    ]
 }
 
 export default App;
